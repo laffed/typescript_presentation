ef8381fa70ff27d28088154d110c79b72c3d5822
laffed <roarklaffed@gmail.com>
2021-06-17 19:04:39 -0500

    First

---
+++ First/index.ts
@@ -0,0 +1,89 @@
+//The basics
+
+// 1. type inference
+const my_unspecific_var = 600;
+my_unspecific_var = "typescript, save me from being a dumb🐴";
+
+
+// 2. Autocompletion 
+/**
+ * We literally program computers that have indefinite memory. 
+ * Let's let them do the hard work.
+ */
+
+type MyAwesomeObj = {
+	without: string;
+	typescript: number[];
+	iDef: {
+		wouldve: "forgotten" | "remembered";
+		allOf: boolean;
+		theKeys: string[];
+	};
+	ofThisObj: boolean | "cheesecake";
+}
+
+// ----define an object of type MyAwesomeObj---- 
+
+
+
+// 3a. Functions
+type FuncVar = (number) => boolean;
+
+function outerFunc(func: FuncVar): string {
+	return func(5) ? "five ain't the one" : "lucky day!";
+}
+
+const innerFunc: FuncVar = num => {
+	return num === 5;
+}
+
+// ----call outerFunc below passing in innerFunc as the parameter----
+
+
+// 3b. Functions cont.
+const x = '29';
+const y = '522';
+let myNumber = 700;
+
+function addToString(a: number, b: number): string {
+	return `${a + b}`;
+}
+
+addToString(x, y); //an example of previous you & TS helping future you out
+
+myNumber = addToString(58, 10); //mutability of types for variables is not allowed, well, because you should never do this
+
+//if you set a type for a var that you're defining from non-hardcoded values,
+//TS will save you from making mistakes like this:
+const myOtherNumber: number = addToString(40, 19) * 2;
+
+
+// 3c. Functions cont.
+function myFunc(num: number): boolean {
+	//TS can help you while you're writing your functions as well
+	if (num) {
+		if (num > 5) {
+			return true;
+		}
+		if (num === 100) {
+			return false;
+		}
+		if (num === 'hello') {
+			return true;
+		}
+		if (5 === 6) {
+			return true;
+		}
+	}
+}
+
+//TODO figure out why this doesn't work
+// const a = [1, 2, 3];
+// const b = [1, 2, 3];
+// function compareArrs(x: number[], y: number[]): boolean {
+// 	if (x === y) {
+// 		return true;
+// 	} else {
+// 		return false;
+// 	}
+// }
